658721680d1f4f3bf4635c4fa1efc19c2153dcfd
KAMARJAHAN <ztenkammu@gmail.com>
2025-07-22 16:08:23 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const backendURL = "https://librespeed-backend.replit.app";
+
+const config = {
+  xhr_dlURL: [`${backendURL}/garbage`],
+  xhr_ulURL: `${backendURL}/upload`,
+  xhr_pingURL: `${backendURL}/garbage`,
+  xhr_dlMultistream: 3,
+  getIp_ispInfo: true,
+  ipURL: "https://ipapi.co/json/",
+};
