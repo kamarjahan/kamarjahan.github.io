7bb5afb5c9330c4a04ffa8ff64670019c1533bfe
KAMARJAHAN <ztenkammu@gmail.com>
2025-07-22 16:17:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const backendURL = "https://librespeed-backend.replit.app";
+const backendURL = "https://ae24a2e9-0bf8-4d92-9380-8fe63556901a-00-3uh1h3ucqs3t4.pike.replit.dev/";
 
 const config = {
   xhr_dlURL: [`${backendURL}/garbage`],
